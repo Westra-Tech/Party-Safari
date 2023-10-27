ffae7c6d0fa0e6f6353549d43c5b7a337ea09617
FabioGunderson <fmg44@scarletmail.rutgers.edu>
2023-10-27 18:30:50 -0400

    added error handling for server

--- API/server.js
+++ API/server.js
@@ -8,3 +8,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+server.on('error', (error) => {
+  console.error(`Server encountered an error: ${error.message}`);
+});
