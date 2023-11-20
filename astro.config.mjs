92e641039a987c18b4314342ceb7f2be6d72fb5b
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2023-11-20 12:52:53 -0500

    Initialize repository

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import svelte from "@astrojs/svelte";
+import prefetch from "@astrojs/prefetch";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), prefetch(), tailwind()]
+});
